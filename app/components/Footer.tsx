380d4e0e0573c48eda44b80481ca581d2a70fdd5
Jamie Turner <jamwt@convex.dev>
2025-01-05 10:43:31 -0800

    can lazy load the ai quote.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,12 @@
 import { convexQuery } from "@convex-dev/react-query";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { useLocation, useRouter } from "@tanstack/react-router";
 import { api } from "~/convex/_generated/api";
 
 const Footer = () => {
   const location = useLocation();
   const path = location.pathname;
-  const { data: comment } = useSuspenseQuery(
+  const { data: comment } = useQuery(
     convexQuery(api.snide.getSnideComment, { path })
   );
   return (
